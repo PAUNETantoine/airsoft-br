3ec4611cca7e18fd6df540f9ded2114bb4ace3ed
PAUNETantoine <paunet.antoine@gmail.com>
2025-05-27 17:34:15 +0200

    fix: Resolving bugs on the dependencies

--- src/app/marais/page.tsx
+++ src/app/marais/page.tsx
@@ -2,25 +2,28 @@
 
 import "leaflet/dist/leaflet.css"
 import L from "leaflet"
+import { useEffect, useState } from "react"
 
 import { MapGenerator } from "@/ui/components/map-generator"
-import { useState } from "react"
 import { GameMenu } from "@/ui/components/game-menu"
+import { GameInfos } from "@/ui/components/game-infos"
 import { PLACES_MARAIS } from "../../../shared/const/consts-marais"
 import { PlaceType } from "../../../shared/type/types"
-import { GameInfos } from "@/ui/components/game-infos"
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-	iconRetinaUrl: "/leaflet/marker-icon-2x.png",
-	iconUrl: "/leaflet/marker-icon.png",
-	shadowUrl: "/leaflet/marker-shadow.png",
-})
 export default function Marais() {
 	const [disabledFilter, setDisabledFilter] = useState<PlaceType[]>([])
 	const [playerIsOnCircle, setPlayerIsOnCircle] = useState<boolean>(true)
+
+	useEffect(() => {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		delete (L.Icon.Default.prototype as any)._getIconUrl
+
+		L.Icon.Default.mergeOptions({
+			iconRetinaUrl: "/leaflet/marker-icon-2x.png",
+			iconUrl: "/leaflet/marker-icon.png",
+			shadowUrl: "/leaflet/marker-shadow.png",
+		})
+	}, [])
 
 	return (
 		<main className="flex h-full flex-col items-center justify-center lg:gap-6">
@@ -34,7 +37,10 @@ export default function Marais() {
 				battleRoyal={true}
 				setPlayerIsOnCircle={setPlayerIsOnCircle}
 			/>
-			<GameInfos playerIsOnCircle={playerIsOnCircle} battleRoyal={true} />
+			<GameInfos
+				playerIsOnCircle={playerIsOnCircle}
+				battleRoyal={true}
+			/>
 		</main>
 	)
 }
